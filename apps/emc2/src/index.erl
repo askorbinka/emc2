891a6ea8790e11e1e5e8279463202add09245f15
orendzi <olga.golubovskaya@ironsoft.su>
2015-01-31 09:39:56 +0000

    changes config port to 8080

--- apps/emc2/src/index.erl
+++ apps/emc2/src/index.erl
@@ -2,4 +2,13 @@
 -compile(export_all).
 -include_lib("n2o/include/wf.hrl").
 
-main() -> #span{body="Hello EMC2"}.
+main () -> 
+    Body = wf : render ( body () ) , 
+    [ #dtl { file =  "login" ,  
+                 bindings = [ { body , Body } ] } ].
+body () -> 
+    [  #span { text  = "Login: " }, 
+       #textbox  { id  = user }, 
+       #span  {  text  =  "Pass: "  }, 
+       #password  {  id  = pass }, 
+       #button  {  text  = "Login" } ].
